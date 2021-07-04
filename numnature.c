199367da59d9d7959bcb02ee9feace356b384ba5
Harshal2403 <56926517+Harshal2403@users.noreply.github.com>
2021-07-04 18:00:26 +0530

    Add files via upload

---
+++ numnature.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void main()
+{
+    int a;
+
+    printf("Enter a number: ");
+    scanf("%d",&a);
+
+    if (a>0)
+    {
+        printf("%d is positive.\n",a);
+    }
+    else if (a<0)
+    {
+        printf("%d is negative.\n",a);
+    }
+    else
+    {
+        printf("%d is zero.\n",a);
+    }    
+    
+}    
