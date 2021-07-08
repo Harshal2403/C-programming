89494a24d032cc378da591794bfb96229d33bdd3
Harshal2403 <56926517+Harshal2403@users.noreply.github.com>
2021-07-08 23:33:41 +0530

    Add files via upload

---
+++ pattern7.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void main()
+{
+    int i,j,count;
+
+    count=1;
+
+    for (i=1;i<=5;i++)
+    {
+        for (j=1;j<=(2*i)-1;j++)
+        {
+            printf("%d ",count);
+            if(i<=j)
+            {
+                count--;
+            }
+            else
+            {
+                count++;
+            }
+
+        }
+        printf("\n");
+        count=1;
+    }
+    
+}
