14c903249bf3ec4f7a321d598d90420633d6e223
Harshal2403 <56926517+Harshal2403@users.noreply.github.com>
2021-08-01 23:26:20 +0530

    Add files via upload

---
+++ castingdemo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int x = 10;
+    char y = 'b';
+    x = x + y;
+
+    float z = x + 1.0;
+    printf("x = %d & z = %.2f", x, z);
+    return 0;
+}
