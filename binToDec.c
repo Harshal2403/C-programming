cc95cf471006f561c0d4fac39e2203982f770265
Harshal2403 <56926517+Harshal2403@users.noreply.github.com>
2021-08-16 13:08:40 +0530

    Add files via upload

---
+++ binToDec.c
@@ -0,0 +1,22 @@
+//Program to convert binary number to decimal
+#include <stdio.h>
+
+int convert(int);
+
+int main()
+{
+    int binary;
+
+    printf("Enter a binary number: ");
+    scanf("%d", &binary);
+
+    printf("decimal : %d", convert(binary));
+
+    return 0;
+}
+
+int convert(int x)
+{
+
+    return 0;
+}
