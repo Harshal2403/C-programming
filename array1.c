87c54a792a71f4d88111149f5abc2d31340711ec
Harshal2403 <56926517+Harshal2403@users.noreply.github.com>
2021-08-29 22:02:39 +0530

    Add files via upload

---
+++ array1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+  int a[10]={10, 9, 7, 101, 23, 44, 12, 78, 34, 23};
+
+  int i,j,temp;
+
+  for (i = 0; i < 10; i++)
+  {
+    for (j = i+1; j < 10; j++)
+    {
+      if (a[j] < a[i])
+      {
+        temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+      }
+      
+    }
+    
+  }
+  printf("Sorted array: ");
+
+  for (i = 0; i < 10; i++)
+  {
+    printf("%d  ",a[i]);
+  }
+  
+  return 0;
+}
